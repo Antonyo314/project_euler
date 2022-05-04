5179747c926c0640598734b22b7d9f71b3f7ae56
Antonyo314 <makarenko.antonf2c@gmail.com>
2022-05-04 13:58:52 +0300

    add problem 45 solution refactoring

--- pr45.py
+++ pr45.py
@@ -1,9 +1,9 @@
 N = 100_000
-triangles = [int(n*(n+1)/2) for n in range(N)]
-pentagonals = [int(n*(3*n-1)/2) for n in range(N)]
-hexagonals = [int(n*(2*n-1)) for n in range(N)]
+triangles = [int(n * (n + 1) / 2) for n in range(N)]
+pentagonals = [int(n * (3 * n - 1) / 2) for n in range(N)]
+hexagonals = [int(n * (2 * n - 1)) for n in range(N)]
 
 res = [i for i in set(triangles).intersection(pentagonals).intersection(hexagonals)]
 res.sort()
-res = [i for i in res if i >40755]
+res = [i for i in res if i > 40755]
 print(res[0])
