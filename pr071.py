1bf801544325f0cdfd59a7931b0abaa9d8e466a9
Antonyo314 <makarenko.antonf2c@gmail.com>
2022-07-03 10:44:48 +0300

    refactoring

--- pr071.py
+++ pr071.py
@@ -3,14 +3,14 @@ from tqdm import trange
 N = 10 ** 6
 
 d = dict()
-for nominator in trange(1, N):
-    denominator = nominator * 7 / 3
+for numinator in trange(1, N):
+    denominator = numinator * 7 / 3
     if int(denominator) != denominator:
         denominator = int(denominator) + 1
     if denominator > N:
         break
-    if nominator / denominator < 3 / 7:
-        d[nominator / denominator] = f'{nominator}/{denominator}'
+    if numinator / denominator < 3 / 7:
+        d[numinator / denominator] = f'{numinator}/{denominator}'
 
 # print(d[max(d.keys())])
 print(d[max(d.keys())].split('/')[0])
