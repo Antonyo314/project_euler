c62b21bc048cbb138175068cf2dbd26f7e5f8beb
Antonyo314 <makarenko.antonf2c@gmail.com>
2022-07-03 10:49:07 +0300

    fix mistake (same answer)

--- pr071.py
+++ pr071.py
@@ -5,12 +5,11 @@ N = 10 ** 6
 d = dict()
 for numinator in trange(1, N):
     denominator = numinator * 7 / 3
-    if int(denominator) != denominator:
-        denominator = int(denominator) + 1
+    denominator = int(denominator) + 1
     if denominator > N:
         break
     if numinator / denominator < 3 / 7:
         d[numinator / denominator] = f'{numinator}/{denominator}'
 
-# print(d[max(d.keys())])
+print(d[max(d.keys())])
 print(d[max(d.keys())].split('/')[0])
